33b9eba6deca38051cb4673d3eaafca813737aa6
Joshua <Joshua.Nezianya@gmail.com>
2016-04-26 09:04:55 -0400

    updated typo

--- app/controllers/cheerups_controller.rb
+++ app/controllers/cheerups_controller.rb
@@ -1,19 +1,34 @@
 class CheerupsController < ApplicationController
-#   validates :title, presence: true,
-#                     length: { minimum: 1, maximum: 140 }
-# end
 
   def index
     @cheerups = Cheerup.all
+  end
+
+  def edit
+    @cheerup = Cheerup.find(params[:id])
   end
 
 
   def create
     @cheerup = Cheerup.new(cheerup_params)
 
-    @cheerup.save
-    redirect_to cheerups_path
+    if @cheerup.save
+      redirect_to cheerups_path
+    else
+      render 'new'
+    end
   end
+
+  def update
+    @cheerup = Cheerup.find(params[:id])
+
+    if @cheerup.update(cheerup_params)
+      redirect_to cheerups_path
+    else
+      render 'edit'
+    end
+  end
+
 
   def new
     @cheerup = Cheerup.new
@@ -21,6 +36,13 @@ class CheerupsController < ApplicationController
 
   def show
     @cheerup = Cheerup.find(params[:id])
+  end
+
+  def destroy
+  @cheerup = Cheerup.find(params[:id])
+  @cheerup.destroy
+
+  redirect_to cheerups_path
   end
 
 
--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,20 @@
 class CommentsController < ApplicationController
+  def create
+    @cheerup = Cheerup.find(params[:cheerup_id])
+    @comment = @cheerup.comments.create(comment_params)
+    redirect_to cheerup_path(@cheerup)
+  end
+
+
+  def destroy
+    @cheerup = Cheerup.find(params[:article_id])
+    @comment = @cheerup.comments.find(params[:id])
+    @comment.destroy
+    redirect_to article_path(@cheerup)
+  end
+
+  private
+    def comment_params
+      params.require(:comment).permit(:commenter, :body)
+    end
 end
